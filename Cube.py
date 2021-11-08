a2ada742d638536d246529803b3ca499d458984e
Cezar Todirisca <cezartodirisca@gmail.com>
2021-11-08 02:26:05 +0200

    Added forming of the cross on the up face

--- Cube.py
+++ Cube.py
@@ -128,21 +128,131 @@ class RubikCube:
 
         self.faces = faces_copy[:]
 
+    def front_to_up_right(self, down_color : Color):
+        self.make_rotation(FaceDirection.FRONT, True)
+        while self.faces[FaceDirection.UP.value].face_matrix[1][2] == down_color:
+            self.make_rotation(FaceDirection.UP, True)
+        self.make_rotation(FaceDirection.RIGHT, True)
+
+    def check_up_color(self, face):
+        if face == FaceDirection.RIGHT:
+            return self.faces[FaceDirection.UP.value].face_matrix[1][2]
+        
+        if face == FaceDirection.BACK:
+            return self.faces[FaceDirection.UP.value].face_matrix[0][1]
+        
+        if face == FaceDirection.LEFT:
+            return self.faces[FaceDirection.UP.value].face_matrix[1][0]
+
+        if face == FaceDirection.FRONT:
+            return self.faces[FaceDirection.UP.value].face_matrix[2][1]
+
+    def third_layer_to_up(self, face, down_color):
+        
+        if face == FaceDirection.FRONT:
+            self.front_to_up_right(down_color)
+        else:
+            difference = face.value - FaceDirection.FRONT.value
+            if difference > 0:
+                for i in range(difference):
+                    self.make_rotation(FaceDirection.UP, True)
+            else:
+                self.make_rotation(FaceDirection.UP, False)
+            self.front_to_up_right(down_color)
+
+    def form_up_cross(self):
+        up_color = self.faces[FaceDirection.UP.value].face_color
+        down_color = self.faces[FaceDirection.DOWN.value].face_color
+
+        #Locate where the edges with the color from the bottom face are
+        positions = [(1,0),(2,1),(1,2),(0,1)]
+        color_number = 0
+        for position in positions:
+            if self.faces[FaceDirection.UP.value].face_matrix[position[0]][position[1]] == down_color:
+                color_number += 1
+
+        if color_number == 4:
+            return
+
+        sides = [FaceDirection.LEFT, FaceDirection.FRONT, FaceDirection.RIGHT, FaceDirection.BACK]
+        
+        while color_number < 4:
+            for face in sides:
+                if self.faces[face.value].face_matrix[0][2] == down_color:
+                    self.third_layer_to_up(face, down_color)
+                    color_number += 1
+
+                if self.faces[face.value].face_matrix[1][0] == down_color:
+                    while self.check_up_color(face) == down_color:
+                        self.make_rotation(FaceDirection.UP,True)
+                    self.make_rotation(face,True)
+                    color_number += 1
+
+                if self.faces[face.value].face_matrix[1][0] == down_color:
+                    while self.check_up_color(face) == down_color:
+                        self.make_rotation(FaceDirection.UP,True)
+
+                    self.make_rotation(face,True)
+                    self.make_rotation(face,True)
+                    self.third_layer_to_up(face, down_color)
+                    color_number += 1
+            
+            if self.faces[FaceDirection.DOWN.value].face_matrix[0][1] == down_color:
+                while self.check_up_color(FaceDirection.FRONT) == down_color:
+                    self.make_rotation(FaceDirection.UP,True)
+
+                self.make_rotation(FaceDirection.FRONT,True)
+                self.make_rotation(FaceDirection.FRONT,True)
+                color_number += 1
+            
+            if self.faces[FaceDirection.DOWN.value].face_matrix[1][0] == down_color:
+                while self.check_up_color(FaceDirection.LEFT) == down_color:
+                    self.make_rotation(FaceDirection.UP,True)
+                    
+                self.make_rotation(FaceDirection.LEFT,True)
+                self.make_rotation(FaceDirection.LEFT,True)
+                color_number += 1
+            
+            if self.faces[FaceDirection.DOWN.value].face_matrix[1][2] == down_color:
+                while self.check_up_color(FaceDirection.RIGHT) == down_color:
+                    self.make_rotation(FaceDirection.UP,True)
+                    
+                self.make_rotation(FaceDirection.RIGHT,True)
+                self.make_rotation(FaceDirection.RIGHT,True)
+                color_number += 1
+
+            if self.faces[FaceDirection.DOWN.value].face_matrix[2][1] == down_color:
+                while self.check_up_color(FaceDirection.BACK) == down_color:
+                    self.make_rotation(FaceDirection.UP,True)
+                    
+                self.make_rotation(FaceDirection.BACK,True)
+                self.make_rotation(FaceDirection.BACK,True)
+                color_number += 1
+                        
+
+    def solve_bottom_layer(self):
+        self.form_up_cross()
+
+    def solve(self):
+        '''
+        The solving algorithm is an easy sequence of steps
+        '''
+        self.solve_bottom_layer()
 
 def main():
     try:
         rubik_cube= RubikCube([
-            Color.ORANGE, Color.BLUE, Color.WHITE, Color.YELLOW, Color.WHITE, Color.WHITE, Color.GREEN, Color.RED, Color.GREEN,
-            Color.YELLOW, Color.ORANGE, Color.ORANGE, Color.GREEN, Color.RED, Color.RED, Color.BLUE, Color.RED, Color.WHITE,
-            Color.YELLOW, Color.YELLOW, Color.WHITE, Color.WHITE, Color.BLUE, Color.ORANGE, Color.RED, Color.WHITE, Color.BLUE,
-            Color.ORANGE, Color.BLUE, Color.BLUE, Color.BLUE, Color.ORANGE, Color.YELLOW, Color.YELLOW, Color.WHITE, Color.YELLOW,
-            Color.ORANGE, Color.YELLOW, Color.BLUE, Color.GREEN, Color.GREEN, Color.RED, Color.GREEN, Color.GREEN, Color.WHITE,
-            Color.GREEN, Color.GREEN, Color.RED, Color.BLUE, Color.YELLOW, Color.ORANGE, Color.RED, Color.ORANGE, Color.RED
+            Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE, Color.WHITE,
+            Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED, Color.RED,
+            Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE, Color.BLUE,
+            Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE, Color.ORANGE,
+            Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN, Color.GREEN,
+            Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW, Color.YELLOW
         ])
     except utils.InvalidCubeConfiguration as cube_exception:
         print(cube_exception)
 
-    rubik_cube.make_rotation(FaceDirection.RIGHT, False)
+    rubik_cube.solve()
 
     for face in rubik_cube.faces:
         for row in face.face_matrix:
