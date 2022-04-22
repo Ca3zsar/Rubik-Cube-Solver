6a69c026a8d6e02710db89825f14d8a763c2e418
Cezar Todirisca <cezartodirisca@gmail.com>
2022-04-22 21:47:41 +0300

    Added  corner detection and indexing

--- database/db_fill.py
+++ database/db_fill.py
@@ -1,5 +1,6 @@
-from cube import CubeCompact
+from cube import CubeCompact, Color
 from collections import deque
+
 directions = {
     CubeCompact.up_rotate: 0,
     CubeCompact.down_rotate: 1,
@@ -22,6 +23,65 @@ directions = {
 }
 
 opposite_faces = [1, 0, 3, 2, 5, 4]
+
+corners = [
+    {Color.RED, Color.BLUE, Color.WHITE},  # ULF
+    {Color.RED, Color.GREEN, Color.WHITE},  # URF
+    {Color.ORANGE, Color.BLUE, Color.WHITE},  # DLF
+    {Color.ORANGE, Color.GREEN, Color.WHITE},  # DRF
+    {Color.RED, Color.BLUE, Color.YELLOW},  # ULB
+    {Color.RED, Color.GREEN, Color.YELLOW},  # URB
+    {Color.ORANGE, Color.BLUE, Color.YELLOW},  # DLB
+    {Color.ORANGE, Color.GREEN, Color.YELLOW}  # DRB
+]
+
+
+def get_corner_orientation(corner):
+    if corner[0] == Color.RED or corner[0] == Color.ORANGE:
+        return 0
+    try:
+        found = corners.index(set(corner))
+        if found == 4 or found == 1 or found == 2 or found == 7:
+            if corner[1] == Color.RED or corner[1] == Color.ORANGE:
+                return 1
+            else:
+                return 2
+        else:
+            if corner[2] == Color.RED or corner[2] == Color.ORANGE:
+                return 1
+            else:
+                return 2
+    except ValueError:
+        print("Corner not found")
+
+
+def get_corner_position(corner):
+    corner = set(corner)
+    try:
+        return corners.index(corner)
+    except ValueError:
+        print("Corner not found")
+        return
+
+
+def get_corner_colors(index, faces):
+    match index:
+        case 0:
+            return ((faces[0] >> 32) >> 4) & 0xF, ((faces[0] & 0xFFFFFFFF) >> 20) & 0xF, ((faces[1] >> 32) >> 28) & 0xF
+        case 1:
+            return ((faces[0] >> 32) >> 12) & 0xF, ((faces[1] & 0xFFFFFFFF) >> 28) & 0xF, ((faces[1] >> 32) >> 20) & 0xF
+        case 2:
+            return (((faces[2] & 0xFFFFFFFF) >> 28) & 0xF), ((faces[0] & 0xFFFFFFFF) >> 12) & 0xF, ((faces[1] >> 32) >> 4) & 0xF
+        case 3:
+            return (((faces[2] & 0xFFFFFFFF) >> 20) & 0xF), ((faces[1] & 0xFFFFFFFF) >> 4) & 0xF, ((faces[1] >> 32) >> 12) & 0xF
+        case 4:
+            return (((faces[0] >> 32) >> 28) & 0xF), ((faces[0] & 0xFFFFFFFF) >> 28) & 0xF, ((faces[2] >> 32) >> 20) & 0xF
+        case 5:
+            return (((faces[0] >> 32) >> 20) & 0xF), ((faces[1] & 0xFFFFFFFF) >> 20) & 0xF, ((faces[2] >> 32) >> 28) & 0xF
+        case 6:
+            return (((faces[2] & 0xFFFFFFFF) >> 4) & 0xF), ((faces[0] & 0xFFFFFFFF) >> 4) & 0xF, ((faces[2] >> 32) >> 12) & 0xF
+        case 7:
+            return (((faces[2] & 0xFFFFFFFF) >> 12) & 0xF), ((faces[1] & 0xFFFFFFFF) >> 12) & 0xF, ((faces[2] >> 32) >> 4) & 0xF
 
 
 def cube_bfs(cube: CubeCompact, max_depth: int = 11):
