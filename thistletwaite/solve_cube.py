9b856c6efa1067fef1c1320d0f4763819d1693e6
Cezar Todirisca <cezartodirisca@gmail.com>
2022-06-10 13:24:35 +0300

    sped up solving algorithm

--- thistletwaite/solve_cube.py
+++ thistletwaite/solve_cube.py
@@ -1,5 +1,6 @@
 import random
 from time import perf_counter
+import collections
 
 face_names = ["U", "D", "F", "B", "L", "R"]
 affected_cubies = [[0, 1, 2, 3, 0, 1, 2, 3], [4, 7, 6, 5, 4, 5, 6, 7], [0, 9, 4, 8, 0, 3, 5, 4],
@@ -16,6 +17,8 @@ phase_moves = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
 def move_str(move):
     return face_names[move // 3] + {1: '', 2: '2', 3: "'"}[move % 3 + 1]
 
+def inverse_move(move):
+    return move + 2 - 2 * (move % 3)
 
 class CubeState:
     def __init__(self, state, route=None):
@@ -70,59 +73,84 @@ class CubeState:
 
 def test(test_state=None, debug=False):
     goal_state = CubeState(list(range(20)) + 20 * [0])
+
     if test_state:
         state = CubeState(test_state.permutation)
-        # print(test_state)
+        print(test_state)
     else:
         state = CubeState(goal_state.state[:])
         moves = [random.randint(0, 17) for _ in range(30)]
-        # print(' '.join([move_str(move) for move in moves]) + '\n')
+        print(' '.join([move_str(move) for move in moves]) + '\n')
 
         for move in moves:
             state = state.apply_move(move)
 
     state.route = []
+    SOLUTION = []
 
     for phase in range(4):
         current_id, goal_id = state.id_(phase), goal_state.id_(phase)
-        states = [state]
-        state_ids = {current_id}
-        if current_id != goal_id:
-            phase_ok = False
-            while not phase_ok:
-                next_states = []
-                possible_variants = []
-                for cur_state in states:
-                    for move in phase_moves[phase]:
-                        if cur_state.route and move // 3 == cur_state.route[-1] // 3:
-                            continue
+        
+        if current_id == goal_id:
+            continue
 
-                        if len(cur_state.route) >= 2 and cur_state.route[-1] // 6 == cur_state.route[-2] // 6 and move // 3 == cur_state.route[-2] // 3:
-                            continue
+        predecessor = {}
+        last_move = {}
+        direction = {}
 
-                        next_state = cur_state.apply_move(move)
-                        next_id = next_state.id_(phase)
-                        if next_id == goal_id:
-                            possible_variants.append(next_state)
+        direction[current_id] = 1
+        direction[goal_id] = 2
 
-                            phase_ok = True
-                        if next_id not in state_ids:
-                            state_ids.add(next_id)
-                            next_states.append(next_state)
+        states = collections.deque()
+        states.append(state)
+        states.append(goal_state)
 
-                if possible_variants:
-                    found = False
-                    for cur_state in possible_variants:
-                        if cur_state.state == goal_state.state:
-                            state = cur_state
-                            found = True
-                            break
-                    if not found:
-                        state = random.choice(possible_variants)
+        phase_ok = False
+        while not phase_ok:
+            old_state = states.popleft()
+            old_id = old_state.id_(phase)
+            old_dir = direction.get(old_id)
 
-                states = next_states
-    # return ' '.join([move_str(m) for m in state.route]) + ' (%d moves)' % len(state.route)
-    return state.route
+            for move in phase_moves[phase]:
+                next_state = old_state.apply_move(move)
+                next_id = next_state.id_(phase)
+                new_dir = direction.get(next_id, 0)
+
+                if new_dir and new_dir != old_dir:
+                    if old_dir > 1:
+                        aux = next_id
+                        next_id = old_id
+                        old_id = aux
+                        move = inverse_move(move)
+                    
+                    moves = [move]
+                    while old_id != current_id:
+                        moves.insert(0, last_move[old_id])
+                        old_id = predecessor[old_id]
+
+                    while next_id != goal_id:
+                        moves.append(inverse_move(last_move[next_id]))
+                        next_id = predecessor[next_id]
+                    
+                    for move in moves:
+                        SOLUTION.append(move)
+                        state = state.apply_move(move)
+                    
+                    phase_ok = True
+                    break
+
+                if not new_dir:
+                    states.append(next_state)
+                    direction[next_id] = old_dir
+                    predecessor[next_id] = old_id
+                    last_move[next_id] = move
+
+            if phase_ok:
+                break
+
+    print(len(SOLUTION))
+    print(' '.join(map(move_str,SOLUTION)))
+        
 
 
 if __name__ == "__main__":
